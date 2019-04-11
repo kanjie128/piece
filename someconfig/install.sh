fb35c562e882c077bd258afc4245c0ca25e3e3e6
jaykan <jaykan@tencent.com>
2019-04-11 22:57:17 +0800

    update

---
+++ someconfig/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#vim config
+cp ./vimrc ~/.vimrc
+
+#config .vim
+tar zxvf vim.tgz
+cp ./root/.vim ~/.vim -R
+rm ./root -rf
+
+#install ctags
+tar zxvf ctags-5.8.tgz
+cd ctags-5.8
+./configure && make && make install
+cd -
+rm ctags-5.8 -rf
+
+#taglist config
+cp ./ctags.cnf ~/.ctags
+
+#install go tools
+#export http_proxy
+#export https_proxy
+go get -u github.com/zmb3/gogetdoc
+go get -u golang.org/x/tools/cmd/guru
+go get -u github.com/golang/lint/golint
+go get -u github.com/davidrjenni/reftools/cmd/fillstruct
+go get -u github.com/rogpeppe/godef
+go get -u github.com/fatih/motion
+go get -u github.com/kisielk/errcheck
+go get -u github.com/derekparker/delve/cmd/dlv
+#go get -u github.com/nsf/gocode
+go get -u github.com/stamblerre/gocode
+
+
+
